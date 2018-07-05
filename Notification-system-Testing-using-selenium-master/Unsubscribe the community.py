e66182a81943a669df4b610d497c1dbcb3d552f0
singhalshubh <shubhendrapalsinghal@gmail.com>
2018-07-05 17:14:51 +0530

    Testing revised Final

---
+++ Notification-system-Testing-using-selenium-master/Unsubscribe the community.py
@@ -0,0 +1,42 @@
+__author__= 'shubh'
+import unittest
+from decouple import config
+from selenium import webdriver
+
+class signup(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		cls.driver = webdriver.Firefox()
+
+	def login(self,var,driver):
+		driver.get("http://" + config('IP_ADDRESS')+ ":" + config('NOTIFICATION_PORT'))
+		driver.find_element_by_xpath('//a [@href="/login/?next=/"]').click()
+		driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/login/?next=/')
+		elem = driver.find_element_by_id("id_username")
+		user = config('NOTIFICATION_USER').split(',')
+		elem.send_keys(user[var])
+		elem = driver.find_element_by_id("id_password")
+		elem.send_keys(config('NOTIFICATION_PASSWORD'))
+		driver.find_element_by_class_name('btn-block').click()
+
+	def test_draftToVisisbleState(self):	
+		driver = webdriver.Firefox()
+		for i in range(1,4):
+			self.login(i,driver)
+			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
+			driver.find_element_by_xpath('//a [@href="/community-view/2/"]').click()
+			driver.find_element_by_id("Unsubscribe").click()
+			driver.find_element_by_id("Yes").click()
+			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/notifications/')
+			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/communities/')
+			driver.find_element_by_xpath('//a [@href="/community-view/2/"]').click()
+			driver.find_element_by_xpath('//a [@href="/community_feed/2/"]').click()
+			#make the id as visible of the button of visible in html file
+			driver.get("http://" + config('IP_ADDRESS') + ":" + config('NOTIFICATION_PORT') + '/logout/')
+			
+	@classmethod
+	def tearDown(cls):
+		cls.driver.quit()
+
+if __name__ == '__main__':
+	unittest.main()
