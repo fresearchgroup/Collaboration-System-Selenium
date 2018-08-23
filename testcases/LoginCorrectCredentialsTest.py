d9635a8cb8e6fa1592a6853fa5e2be7927dce27e
anjalysuresh <anjalysuresh@yahoo.co.in>
2018-08-23 21:48:14 +0530

    Update LoginCorrectCredentialsTest.py

--- testcases/LoginCorrectCredentialsTest.py
+++ testcases/LoginCorrectCredentialsTest.py
@@ -13,8 +13,8 @@ class LoginCorrect(unittest.TestCase):
         
 
     def test_LoginCorrect(self):
-        user ="testuser"
-        pwd= "collaborative"
+        user ="honey"
+        pwd= "root1234"
         driver = webdriver.Remote(command_executor='http://10.129.132.104:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX) 
 
         #driver = webdriver.Firefox()
